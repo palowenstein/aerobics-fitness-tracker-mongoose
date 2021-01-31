bda6bcca4f496f04620ebe105ac491b7a1cd557a
abbotkinneydude <pa@soundtrackspecialist.com>
2021-01-30 17:10:17 -0800

    updated db, localhost, seed to aerobics, compacted code

--- application/seeders/seed.js
+++ application/seeders/seed.js
@@ -1,142 +1,79 @@
+// Requires mongoose for mongoDB database connection
 let mongoose = require("mongoose");
+
+// Require models for database structure
 let db = require("../models");
 
-mongoose.connect("mongodb://localhost/workout", {
+mongoose.connect("mongodb://localhost/aerobics", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
 
-let workoutSeed = [
+let aerobicsSeed = [
   {
     day: new Date().setDate(new Date().getDate()-10),
     exercises: [
-      {
-        type: "resistance",
-        name: "Bicep Curl",
-        duration: 20,
-        weight: 100,
-        reps: 10,
-        sets: 4
-      }
+      { type: "resistance", name: "Bicep Curl", duration: 20, weight: 100, reps: 10, sets: 4 }
     ]
   },
   {
     day: new Date().setDate(new Date().getDate()-9),
     exercises: [
-      {
-        type: "resistance",
-        name: "Lateral Pull",
-        duration: 20,
-        weight: 300,
-        reps: 10,
-        sets: 4
-      }
+      { type: "resistance", name: "Lateral Pull", duration: 20, weight: 300, reps: 10, sets: 4 }
     ]
   },
   {
     day: new Date().setDate(new Date().getDate()-8),
     exercises: [
-      {
-        type: "resistance",
-        name: "Push Press",
-        duration: 25,
-        weight: 185,
-        reps: 8,
-        sets: 4
-      }
+      { type: "resistance", name: "Push Press", duration: 25, weight: 185, reps: 8, sets: 4 }
     ]
   },
   {
     day: new Date().setDate(new Date().getDate()-7),
     exercises: [
-      {
-        type: "cardio",
-        name: "Running",
-        duration: 25,
-        distance: 4
-      }
+      { type: "cardio", name: "Running", duration: 25, distance: 4 }
     ]
   },
   {
     day: new Date().setDate(new Date().getDate()-6),
     exercises: [
-      {
-        type: "resistance",
-        name: "Bench Press",
-        duration: 20,
-        weight: 285,
-        reps: 10,
-        sets: 4
-      }
+      { type: "resistance",  name: "Bench Press", duration: 20, weight: 285, reps: 10, sets: 4 }
     ]
   },
   {
     day: new Date().setDate(new Date().getDate()-5),
     exercises: [
-      {
-        type: "resistance",
-        name: "Bench Press",
-        duration: 20,
-        weight: 300,
-        reps: 10,
-        sets: 4
-      }
+      { type: "resistance", name: "Bench Press", duration: 20, weight: 300, reps: 10, sets: 4 }
     ]
   },
   {
     day: new Date().setDate(new Date().getDate()-4),
     exercises: [
-      {
-        type: "resistance",
-        name: "Quad Press",
-        duration: 30,
-        weight: 300,
-        reps: 10,
-        sets: 4
-      }
+      { type: "resistance", name: "Quad Press", duration: 30, weight: 300, reps: 10, sets: 4 }
     ]
   },
   {
     day: new Date().setDate(new Date().getDate()-3),
     exercises: [
-      {
-        type: "resistance",
-        name: "Bench Press",
-        duration: 20,
-        weight: 300,
-        reps: 10,
-        sets: 4
-      }
+      { type: "resistance", name: "Bench Press", duration: 20, weight: 300, reps: 10, sets: 4 }
     ]
   },
   {
     day: new Date().setDate(new Date().getDate()-2),
     exercises: [
-      {
-        type: "resistance",
-        name: "Military Press",
-        duration: 20,
-        weight: 300,
-        reps: 10,
-        sets: 4
-      }
+      { type: "resistance", name: "Military Press", duration: 20, weight: 300, reps: 10, sets: 4 }
     ]
   },
   {
     day: new Date().setDate(new Date().getDate()-1),
     exercises: [
-      {
-        type: "resistance",
-        name: "Bench",
-        duration: 30,
-        distance: 2
-      }
+      { type: "resistance", name: "Bench", duration: 30, distance: 2 }
     ]
   }
 ];
 
-db.Workout.deleteMany({})
-  .then(() => db.Workout.collection.insertMany(workoutSeed))
+db.Aerobics.deleteMany({})
+  .then(() => db.Aerobics.collection.insertMany(aerobicsSeed))
   .then(data => {
     console.log(data.result.n + " records inserted!");
     process.exit(0);
